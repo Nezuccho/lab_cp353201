f0852a9a4bb5dbecc01d7ef702b0a61f93078f70
Nezuccho <piratiq2548@gmail.com>
2025-08-04 23:09:04 +0700

    Update Lab7

---
+++ Lab7/CountWordClumps/src/sqa/test/TestCountWordClumps.java
@@ -0,0 +1,31 @@
+package sqa.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import sqa.main.CountWordClumps;
+
+class TestCountWordClumps {
+
+	@ParameterizedTest
+	@MethodSource("BranchAndConditionCoverageTestCase")
+	void Test_CountClumpsBranchAndConditionCoverage(int[] nums, int expectedResult) {
+		assertEquals(expectedResult, CountWordClumps.countClumps(nums));
+	}
+
+	static Stream<Arguments>BranchAndConditionCoverageTestCase() {
+		return Stream.of(
+			Arguments.of(null, 0),                      
+			Arguments.of(new int[] {}, 0),              
+			Arguments.of(new int[] {1, 1, 3, 3}, 2), 
+			Arguments.of(new int[] {2, 2, 2}, 1)
+		);
+	}
+
+}
